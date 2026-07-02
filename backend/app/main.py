7a86353b73d9bb85f41c58183cffae4b71c80668
upendra80026 <161405475+upendra80026@users.noreply.github.com>
2026-07-02 11:47:34 +0530

    Complete Milestone 1: FastAPI, Docker, PostgreSQL setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,22 @@
 from fastapi import FastAPI
 from app.db.database import engine, Base
 from app.models import user
+from app.api.auth import routes as auth_routes
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="Research Funding & Innovation Intelligence Platform"
 )
-
-
+app.include_router(
+    auth_routes.router,
+    prefix="/api/auth",
+    tags=["Auth"]
+)
 @app.get("/")
 def home():
     return {
         "message": "Platform Running"
     }
-
-
 @app.get("/health")
 def health():
     return {
